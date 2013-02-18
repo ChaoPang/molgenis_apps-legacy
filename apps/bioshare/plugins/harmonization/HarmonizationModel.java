1c3d994044f2c554336eb07a4955193b6446c1e6
Chao Pang <ChaoPang229@gmail.com>
2013-02-18 13:12:26 +0100

    using 3 ontologies as the external knowledge to expand the terms

--- apps/bioshare/plugins/harmonization/HarmonizationModel.java
+++ apps/bioshare/plugins/harmonization/HarmonizationModel.java
@@ -52,8 +52,8 @@ public class HarmonizationModel extends EasyPluginModel
 
 	// private String[] ontologies = { "1351", "1136", "1353", "2018", "1032" };
 	private String[] ontologies =
-	{ "1032", "1427", "1353" };
-
+	{ "1353", "1032", "1427" };
+	// "1032", "1427",
 	private List<String> ontologyAccessions = Arrays.asList(ontologies);
 
 	private String freeMakerTemplate = "Harmonization.ftl";
