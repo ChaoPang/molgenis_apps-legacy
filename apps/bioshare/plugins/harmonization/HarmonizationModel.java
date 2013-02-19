24bae337ab086492f57a3d93634c89c2a9db08e9
Chao Pang <ChaoPang229@gmail.com>
2013-02-19 17:38:03 +0100

    added one more ontology to expand terms

--- apps/bioshare/plugins/harmonization/HarmonizationModel.java
+++ apps/bioshare/plugins/harmonization/HarmonizationModel.java
@@ -36,8 +36,7 @@ public class HarmonizationModel extends EasyPluginModel
 	private Map<Integer, PredictorInfo> predictors = null;
 	// private String[] ontologies = { "1351", "1136", "1353", "2018", "1032" };
 	private String[] ontologies =
-	// { "1353", "1032", "1427" };
-	{ "1353", "1032" };
+	{ "1353", "1032", "1427" };
 	private List<String> ontologyAccessions = Arrays.asList(ontologies);
 	private String freeMakerTemplate = "Harmonization.ftl";
 	private int totalJobs = 0;
